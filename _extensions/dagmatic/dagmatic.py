4a04d145189dc94d48409ad2063dfc5fe0fd98b4
Sean Farley <sean@farley.io>
2015-06-08 16:59:04 -0700

    asciidag: add dagmatic dependency

---
+++ _extensions/dagmatic/dagmatic.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import re
+import cStringIO
+
+from nodes import TransitionText, Node, Style
+from edges import types
+
+# We're looking for node labels (runs of alphanumeric chars) and the edges
+# between them. E.g. given a line like "  \ a-b  :", the tokens of interest
+# are \, a, -, b, :. This regex is a good place to start (but now we have
+# two problems).
+nodefind_re = re.compile(r'([a-zA-Z0-9\'^]+)')
+
+
+def parse(text):
+    '''Read a sequence of lines. Return a DAGList.
+    '''
+    # First step: turn input lines into a "grid" of character cells.
+    # grid[i][j] tells us what is occupying cell (i,j): either a node
+    # or a single non-node character.
+    text = text.splitlines()
+    grid = _read_grid(text)
+    nodes = []
+
+    # Now turn the grid into an AST-like thing: the DAGList.
+    for (row, line) in enumerate(grid):
+        for (col, ch) in enumerate(line):
+            ch.parse(nodes, grid, row, col)
+
+    nodemap = {str(node): node for node in nodes}
+    return DAG(nodemap)
+
+
+def _read_grid(infile):
+    grid = []
+    style = ''
+    for line in infile:
+        grid.append([])
+        currow = grid[-1]
+        if line.lstrip().startswith('||'):
+            ws, text = line.split('||')
+            currow += [types[c] for c in ws]
+            currow += [types['||'], TransitionText(text.strip())]
+        elif line.lstrip().startswith('{') or style:
+            style += line.strip()
+            if line.rstrip().endswith('}'):
+                nodestyle = Style()
+                # parse the dictionary
+                style = style.strip('{')
+                style = style.strip('}')
+                for kv in style.split(','):
+                    if not kv.strip():
+                        continue
+                    key, val = kv.split(':', 1)
+                    nodestyle[key.strip()] = val.strip()
+                currow += [nodestyle]
+
+                style = ''
+        else:
+            chunks = nodefind_re.split(line.rstrip())
+
+            for (idx, chunk) in enumerate(chunks):
+                if idx % 2 == 1:  # must be a node (run of alphanumeric)
+                    node = Node(chunk)
+                    currow += [node] * len(chunk)
+                else:
+                    # Must preserve every input char because of the visual
+                    # nature of the input language -- need grid[i][j] to be
+                    # useful!
+                    currow += [types[c] for c in chunk]
+    return grid
+
+
+class DAG(object):
+    '''A graph of nodes. Actually this is two overlaid graphs: the traditional
+    DAG of parent/child relationships, and the newfangled directed cyclic
+    graph of obsolescence markers.
+    '''
+    def __init__(self, nodemap):
+        self.nodemap = nodemap              # map node name to Node
+        self._nodes = None
+
+    @property
+    def nodes(self):
+        if self._nodes is None:
+            self._nodes = [n.name for n in self.nodemap.values()]
+        return self._nodes
+
+    def __getitem__(self, name):
+        '''a naive way to get a node: search for the exact name, fallback
+        to searching through all the nodes
+        '''
+        ret = self.nodemap.get(name)
+        if ret is None:
+            for n in self.nodemap.values():
+                if n.name == name:
+                    ret = n
+                    break
+        return ret
+
+    def get_parent_names(self, name):
+        '''return parents of specified node as str (node names)'''
+        return [parent.name
+                for parent in self[name].parents]
+
+    def get_precursor_names(self, name):
+        '''return precursors of specified node as str (node names)'''
+        return [precursor.name
+                for precursor in self[name].precursors]
+
+    def dump(self, outfile):
+        for node in self.nodemap.values():
+            parents = ','.join(str(p) for p in node.parents)
+            obs = ''
+            if node.precursors:
+                precursors = ','.join(str(p) for p in node.precursors)
+                obs = ' (obsoletes %s)' % (precursors,)
+            print('%s[%d, %d] -> %s%s' % (node, node.row, node.col, parents,
+                                          obs), file=outfile)
+
+    def tikz(self, outfile):
+        # need to do two passes so that all nodes are defined first
+        for node in self.nodemap.values():
+            node.tikz(outfile)
+
+        for node in self.nodemap.values():
+            # output the edges
+            for p in node.parents:
+                print(r'\draw[edge] (%s) -- (%s);' % (p, node), file=outfile)
+
+            # output the obsolete edges
+            for p in node.precursors:
+                print(r'\draw[markeredge] (%s) -- (%s);' % (p, node),
+                      file=outfile)
+
+    def tikz_string(self):
+        output = cStringIO.StringIO()
+        self.tikz(output)
+        return output.getvalue()
+
+def main():
+    dag = parse(sys.stdin.read())
+    print('dag:')
+    dag.dump(sys.stdout)
+
+
+if __name__ == '__main__':
+    main()
---
+++ _extensions/dagmatic/test_dagmatic.py
@@ -0,0 +1,133 @@
+import nose.tools as nt
+
+import dagmatic
+
+
+def test_readme_1_simple():
+    input = r'''
+   a-b-c
+      \
+       d-e
+'''
+    dag = _parse_one(input)
+    nt.assert_items_equal(dag.nodes, ['a', 'b', 'c', 'd', 'e'])
+    _assert_parents(dag, 'a', [])
+    _assert_parents(dag, 'b', ['a'])
+    _assert_parents(dag, 'c', ['b'])
+    _assert_parents(dag, 'd', ['b'])
+    _assert_parents(dag, 'e', ['d'])
+
+
+def test_readme_2_obsolescence():
+    input = r'''
+  a-b-c
+   \: :
+    d-e
+'''
+    dag = _parse_one(input)
+    nt.assert_items_equal(dag.nodes, ['a', 'b', 'c', 'd', 'e'])
+    _assert_parents(dag, 'd', ['a'])
+    _assert_precursors(dag, 'a', [])
+    _assert_precursors(dag, 'b', [])
+    _assert_precursors(dag, 'c', [])
+    _assert_precursors(dag, 'd', ['b'])
+    _assert_precursors(dag, 'e', ['c'])
+
+
+def test_merge():
+    input = r'''
+a-b-3-x
+ \ \
+  7-1-f-5
+'''
+    dag = _parse_one(input)
+    _assert_parents(dag, '1', ['b', '7'])
+
+
+def test_multiple_roots():
+    input = r'''
+1-2-3
+     \
+  4-5-6-7
+'''
+    dag = _parse_one(input)
+    _assert_parents(dag, '1', [])
+    _assert_parents(dag, '4', [])
+    _assert_parents(dag, '6', ['3', '5'])
+
+
+def test_the_whole_enchilada():
+    # a merge! multiple roots! obsolescence!
+    input = r'''
+a-b-3-x
+ \ \
+  c-1-f-5
+        :
+        6-7-8
+'''
+    dag = _parse_one(input)
+    _assert_parents(dag, 'a', [])
+    _assert_parents(dag, 'c', ['a'])
+    _assert_parents(dag, 'b', ['a'])
+    _assert_parents(dag, '1', ['c', 'b'])
+    _assert_parents(dag, '6', [])
+    _assert_precursors(dag, 'x', [])
+    _assert_precursors(dag, '6', ['5'])
+    _assert_precursors(dag, '7', [])
+
+def test_text_transition():
+    input = r'''
+  a-b
+
+  || hg commit --amend
+  || (safe, using evolve)
+
+  a-b.c
+   \:
+    b'
+'''
+    dag = _parse_one(input)
+    _assert_parents(dag, 'a', [])
+    _assert_parents(dag, 'b', ['a'])
+    _assert_parents(dag, "b'", ['a'])
+    _assert_parents(dag, 'c', [])
+    _assert_precursors(dag, 'a', [])
+    _assert_precursors(dag, "b'", ['b'])
+    _assert_precursors(dag, 'c', ['b'])
+
+
+def test_diagonal_obsolete_markers():
+    input = r'''
+      f
+      |
+    d-e
+   /
+  a-b-c^T
+   <:>
+    b'
+'''
+    dag = _parse_one(input)
+    _assert_parents(dag, 'a', [])
+    _assert_parents(dag, 'b', ['a'])
+    _assert_parents(dag, 'c', ['b'])
+    _assert_parents(dag, "b'", [])
+    _assert_precursors(dag, "b'", ['a', 'b', 'c'])
+    _assert_obsolete(dag, ['a', 'b', 'c'])
+
+
+def _parse_one(text):
+    return dagmatic.parse(text)
+
+
+def _assert_parents(dag, name, expect):
+    actual = dag.get_parent_names(name)
+    nt.assert_items_equal(actual, expect)
+
+
+def _assert_precursors(dag, name, expect):
+    actual = dag.get_precursor_names(name)
+    nt.assert_items_equal(actual, expect)
+
+def _assert_obsolete(dag, name):
+    for i in name:
+        nt.assert_items_equal([dag[i].obsolete], [True])
